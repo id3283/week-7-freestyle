c32f6cf8d083aec105b85c9e709e26942118ae3b
Dave Dykes <148535937+brightworkdigital@users.noreply.github.com>
2026-05-20 16:41:38 +0100

    added a version of the labeler method that shows how to use the generic type as a variable type and as a return type.

--- src/main/java/generics/Labeler.java
+++ src/main/java/generics/Labeler.java
@@ -5,14 +5,13 @@ public class Labeler {
         // T is inferred from the type of thing we 👆🏼passed in for the second parameter...
         System.out.println(label + ": " + someObject);
 
-
     // 👇🏼this means I can use T as a type for a variable:
         T anotherValue = someObject;
 
         System.out.println("The type of T is: " + someObject.getClass());
     }
 
-    //     👇🏼 I can also use T as a return type
+    //         👇🏼 I can also use T as a return type
     static <T> T displayAndReturnWithLabel(String label, T someObject){
         System.out.println(label + ": " + someObject);
 
